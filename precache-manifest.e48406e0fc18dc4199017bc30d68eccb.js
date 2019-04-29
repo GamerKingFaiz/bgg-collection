174d29da6ef505cb787d9ff8467a0c86804ae4f4
Faiz-ur Rahman <Faiz-ur.Rahman@kofax.com>
2019-04-29 13:45:52 -0700

    Updates

---
+++ precache-manifest.e48406e0fc18dc4199017bc30d68eccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997ecae6a8cc8dd4fb938fb933ba9a73",
+    "url": "/bgg-collection/index.html"
+  },
+  {
+    "revision": "a822d65a1372892d1652",
+    "url": "/bgg-collection/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "d456337b6a80dd5ac53b",
+    "url": "/bgg-collection/static/css/main.74e4d564.chunk.css"
+  },
+  {
+    "revision": "a822d65a1372892d1652",
+    "url": "/bgg-collection/static/js/2.de4419f7.chunk.js"
+  },
+  {
+    "revision": "d456337b6a80dd5ac53b",
+    "url": "/bgg-collection/static/js/main.088798e4.chunk.js"
+  },
+  {
+    "revision": "7200ee5144944743b419",
+    "url": "/bgg-collection/static/js/runtime~main.ec177c57.js"
+  }
+]);
