57d36fd3f1440aa12135b07f457ac47aa0830114
GamerKingFaiz <gamerkingfaiz@gmail.com>
2019-04-30 14:36:03 -0700

    Updates

---
+++ precache-manifest.ddbdd9020b390278b77c1f8a972d0cc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6099b0cc2712447ca685a0e0d6623c37",
+    "url": "/bgg-collection/index.html"
+  },
+  {
+    "revision": "f1f8fbe79f319bfaa978",
+    "url": "/bgg-collection/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "b48e8b9ac833bbc84ad9",
+    "url": "/bgg-collection/static/css/main.fed2397a.chunk.css"
+  },
+  {
+    "revision": "f1f8fbe79f319bfaa978",
+    "url": "/bgg-collection/static/js/2.71b836c5.chunk.js"
+  },
+  {
+    "revision": "b48e8b9ac833bbc84ad9",
+    "url": "/bgg-collection/static/js/main.709aa0d0.chunk.js"
+  },
+  {
+    "revision": "7200ee5144944743b419",
+    "url": "/bgg-collection/static/js/runtime~main.ec177c57.js"
+  }
+]);
