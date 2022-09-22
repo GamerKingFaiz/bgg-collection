3a2780821ed529fbf471d4016cf50743004ae4a6
GamerKingFaiz <gamerkingfaiz@gmail.com>
2022-09-22 14:58:15 -0700

    Updating CORS proxy URL

--- src/utils/urlConstants.js
+++ src/utils/urlConstants.js
@@ -2,5 +2,6 @@
 These URLs are using a self-hosted cors-anywhere reverse proxy to add a CORS header to the BGG API
 Source: https://github.com/Rob--W/cors-anywhere
 *******************************/
-export const THING_ITEMS_ENDPOINT = 'https://cryptic-brushlands-34819.herokuapp.com/https://boardgamegeek.com/xmlapi2/thing?stats=1&id=';
-export const COLLECTION_ENDPOINT = 'https://cryptic-brushlands-34819.herokuapp.com/https://api.geekdo.com/xmlapi2/collection?own=1&stats=1&excludesubtype=boardgameexpansion&username=';
+const CORS_URL = 'https://web-production-de84.up.railway.app'
+export const THING_ITEMS_ENDPOINT = `${CORS_URL}/https://boardgamegeek.com/xmlapi2/thing?stats=1&id=`;
+export const COLLECTION_ENDPOINT = `${CORS_URL}/https://api.geekdo.com/xmlapi2/collection?own=1&stats=1&excludesubtype=boardgameexpansion&username=`;
