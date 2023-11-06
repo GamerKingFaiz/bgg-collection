d66a7fc6b5750ecee2e4ece01d014ab6d8d3e5c6
GamerKingFaiz <gamerkingfaiz@gmail.com>
2023-11-06 12:05:27 -0800

    Remove CORS workaround

--- README.md
+++ README.md
@@ -11,4 +11,3 @@ I only have ~40 games, so it doesn't look different, but try with a bigger colle
 ## Geeky Details:
 The page is built in React, so it's responsive, meaning it will play nice on mobile.<br />
 I'm using the official [BGG XML API](https://boardgamegeek.com/wiki/page/BGG_XML_API2) then the [xml2js library](https://www.npmjs.com/package/xml2js) to convert the XML to JSON.<br />
-Finally, I'm using [cors-anywhere](https://github.com/Rob--W/cors-anywhere) to add a CORS header to the BGG API.<br />
--- src/utils/urlConstants.js
+++ src/utils/urlConstants.js
@@ -1,7 +1,2 @@
-/*******************************
-These URLs are using a self-hosted cors-anywhere reverse proxy to add a CORS header to the BGG API
-Source: https://github.com/Rob--W/cors-anywhere
-*******************************/
-const CORS_URL = 'https://web-production-de84.up.railway.app'
-export const THING_ITEMS_ENDPOINT = `${CORS_URL}/https://boardgamegeek.com/xmlapi2/thing?stats=1&id=`;
-export const COLLECTION_ENDPOINT = `${CORS_URL}/https://api.geekdo.com/xmlapi2/collection?own=1&stats=1&excludesubtype=boardgameexpansion&username=`;
+export const THING_ITEMS_ENDPOINT = 'https://boardgamegeek.com/xmlapi2/thing?stats=1&id=';
+export const COLLECTION_ENDPOINT = 'https://api.geekdo.com/xmlapi2/collection?own=1&stats=1&excludesubtype=boardgameexpansion&username=';
