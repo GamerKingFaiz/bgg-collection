a7b9ef830619616506324185516a8135292d8396
GamerKingFaiz <gamerkingfaiz@gmail.com>
2025-11-25 17:28:37 -0800

    Updated API URL

--- src/utils/urlConstants.js
+++ src/utils/urlConstants.js
@@ -1,7 +1,3 @@
-/*******************************
-These URLs are using a self-hosted cors-anywhere reverse proxy to add a CORS header to the BGG API
-Source: https://github.com/Rob--W/cors-anywhere
-*******************************/
-const CORS_URL = 'https://web-production-de84.up.railway.app'
-export const THING_ITEMS_ENDPOINT = `${CORS_URL}/https://boardgamegeek.com/xmlapi2/thing?stats=1&id=`;
-export const COLLECTION_ENDPOINT = `${CORS_URL}/https://api.geekdo.com/xmlapi2/collection?own=1&stats=1&excludesubtype=boardgameexpansion&username=`;
+const API_BASE_URL = 'https://api.betterbggcollection.com/';
+export const THING_ITEMS_ENDPOINT = `${API_BASE_URL}xmlapi2/thing?stats=1&id=`;
+export const COLLECTION_ENDPOINT = `${API_BASE_URL}xmlapi2/collection?own=1&stats=1&excludesubtype=boardgameexpansion&username=`;
